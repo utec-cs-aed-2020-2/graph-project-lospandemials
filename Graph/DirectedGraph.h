6264a7ebed68f344fc4a73c7edb129da72743156
jneirar <jorge.neira@utec.edu.pe>
2020-11-13 22:32:05 -0500

    feat(undirectedgraph): Add structure of directed graph

--- Graph/DirectedGraph.h
+++ Graph/DirectedGraph.h
@@ -5,39 +5,40 @@
 
 template<typename TV, typename TE>
 class DirectedGraph : public Graph<TV, TE>{
-    DirectedGraph() : Graph<TV, TE>() {
+public:
+    DirectedGraph(){
 
     }
     ~DirectedGraph(){
-
+        this->clear();
     }
 
     bool insertVertex(std::string id, TV vertex) override;
     bool createEdge(std::string id1, std::string id2, TE w) override;
     bool deleteVertex(std::string id) override;
-    bool deleteEdge(std::string id) override;
+    bool deleteEdge(std::string id1, std::string id2) override;
     
     void display();
 };
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::insertVertex(std::string id, TV vertex){
-    return;
+    return true;
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::createEdge(std::string id1, std::string id2, TE w){
-    return;
+    return true;
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::deleteVertex(std::string id){
-    return;
+    return true;
 }
 
 template<typename TV, typename TE>
-bool DirectedGraph<TV, TE>::deleteEdge(std::string id){
-    return;
+bool DirectedGraph<TV, TE>::deleteEdge(std::string id1, std::string id2){
+    return true;
 }
 
 template<typename TV, typename TE>
