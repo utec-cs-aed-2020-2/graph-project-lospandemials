4f6935335f329fe901cc8b9581a78b6a0844a6bb
soupboris <sebastian.berendson@utec.edu.pe>
2020-11-25 14:46:09 -0500

    feat(main.cpp): menu de navegacion de proyecto

--- main.cpp
+++ main.cpp
@@ -2,19 +2,27 @@
 #include "Tester/tester.h"
 #include "Parser/parser.h"
 
-using namespace std;
-
 int main(int argc, char *argv[]) {
     try{
-        std::cout << "================================================" << std::endl;
-        std::cout << "MENU GRAPH TESTER" << std::endl;
-        std::cout << "================================================" << std::endl;
-        
-        Tester::executeExamples();
-        //Tester::executeParser();
-        
-    }catch (invalid_argument& e){
-        cerr << e.what() << endl;
+        int option = 0, option1 = 0;
+        do{
+            console_clear();
+            option = menuMain();
+            switch(option){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+        }while(option != 5);
+    }catch (const std::exception & ex){
+        std::cerr << ex.what() << std::endl;
         return EXIT_FAILURE;
     }
     
