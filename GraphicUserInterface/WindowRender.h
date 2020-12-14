2ee7250542c69b79352339a52d559132ce7e3708
soupboris <sebastian.berendson@utec.edu.pe>
2020-12-14 01:26:51 -0500

    feat(GUI): added vertex and edge drawing functions according to coordinates of airports, removed dummy data and dummy drawing of line and vertex

--- GraphicUserInterface/WindowRender.h
+++ GraphicUserInterface/WindowRender.h
@@ -3,37 +3,32 @@
 
 using namespace std;
 
-using namespace sf;
+void DrawAirport(double longitude, double latitude, sf::RenderWindow* window){
+    sf::CircleShape circle(25);
+    circle.setFillColor(sf::Color::Cyan);
+    circle.setPosition(longitude, latitude);
+    circle.setOrigin(25, 25);
+    window->draw(circle);
+}
+
+void DrawConnection (double longitudeA, double latitudeA, double longitudeB, double latitudeB, sf::RenderWindow* window){
+    sf::Vertex line[] = 
+	{
+		sf::Vertex(sf::Vector2f(longitudeA, latitudeA), sf::Color::Black),
+		sf::Vertex(sf::Vector2f(longitudeB, latitudeB), sf::Color::Black)
+	};
+
+    window->draw(line, 2, sf::Lines);
+}
 
 
-int main(){
+void RenderGraph(){
 sf::RenderWindow* window = new sf::RenderWindow (sf::VideoMode(1000,1000), "Graph Display");
 window->clear(sf::Color(255,255, 255, 255));
 
-sf::Vertex line2[] = 
-	{
-		sf::Vertex(sf::Vector2f(200, 200), sf::Color::Black),
-		sf::Vertex(sf::Vector2f(100, 200), sf::Color::Black)
-	};
 
 
-window->draw(line2, 2, sf::Lines);
 
-int x1 = 100;
-int y1 = 200;
-sf::CircleShape circle(25);
-circle.setFillColor(sf::Color::Cyan);
-circle.setPosition(x1, y1);
-circle.setOrigin(25, 25);
-window->draw(circle);
-
-int x2 = 200;
-int y2 = 200;
-sf::CircleShape circle2(25);
-circle2.setFillColor(sf::Color::Cyan);
-circle2.setPosition(x2, y2);
-circle2.setOrigin(25, 25);
-window->draw(circle2);
 
 sf::Event event;
 bool close = 1;
