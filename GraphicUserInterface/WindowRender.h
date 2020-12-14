5ad73fd2376d28866d54d7bf161cc389d444a039
soupboris <sebastian.berendson@utec.edu.pe>
2020-12-14 13:44:14 -0500

    test(tester.h): added GUI tester for airport graph

--- GraphicUserInterface/WindowRender.h
+++ GraphicUserInterface/WindowRender.h
@@ -33,6 +33,8 @@ void DrawConnection (double longitudeA, double latitudeA, double longitudeB, dou
 
 class GUI{
 
+public: 
+
 void RenderAirportGraph(UnDirectedGraph<Airport, double> &tempGraph);
 
 };
