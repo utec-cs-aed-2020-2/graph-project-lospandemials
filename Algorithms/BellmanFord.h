6934788d9a92b0bdda0ebd91f544609add6ba544
AmaruEscalante <sebastian.escalante@utec.edu.pe>
2020-12-14 12:50:38 -0500

    feature (BellmanFord): added BellmanFord Relax Function

--- Algorithms/BellmanFord.h
+++ Algorithms/BellmanFord.h
@@ -15,5 +15,20 @@ public:
     returnBellmanFordType apply(std::string id);
 };
 
+template<typename TV, typename TE>
+bool BellmanFord<TV, TE>::BellmanFordRelax(distanceUnorderedMapType &distances, parentUnorderedMapType &parents, std::vector<std::string> order){
+    TE maxVal = INF;
+    bool flag = false;
+    for(std::string id : order){
+        for(Edge<TV, TE>* edge : this->vertexes[id]->edges){
+            if(distances[edge->vertexes[0]] != maxVal && distances[edge->vertexes[0]] + edge->weight < distances[edge->vertexes[1]]){
+                distances[edge->vertexes[1]] = distances[edge->vertexes[0]] + edge->weight;
+                parents[edge->vertexes[1]] = edge->vertexes[0];
+                flag = true;
+            }
+        }
+    }
+    return flag;
+}
 
 #endif
