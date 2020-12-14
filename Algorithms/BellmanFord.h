30843402dee1fbfb66ffbe7b10d221106905efbc
AmaruEscalante <sebastian.escalante@utec.edu.pe>
2020-12-14 12:51:32 -0500

    feature (BellmanFord): added BellmanFord graph return

--- Algorithms/BellmanFord.h
+++ Algorithms/BellmanFord.h
@@ -42,5 +42,30 @@ void BellmanFord<TV, TE>::BellmanFordAlgorithm(distanceUnorderedMapType &distanc
         std::cout << "\n.......¡Negative cycle found!.......\n";
 }
 
+template<typename TV, typename TE>
+returnBellmanFordType BellmanFord<TV, TE>::apply(std::string id){
+    distanceUnorderedMapType distances;
+    parentUnorderedMapType parents;
+
+    if(!this->vertexes.count(id)){
+        std::cout << "ERROR: ID not found.\n";
+        return std::make_pair(parents, distances);
+    }
+
+    std::cout << "\nBellman Ford from vertex: " << this->vertexes[id]->data << "\n";
+
+    TE maxVal = INF;
+    std::vector<std::string> order;
+    order.push_back(id);
+    for(auto p : this->vertexes){
+        if(p.first == id)   distances[p.second] = 0;
+        else    distances[p.second] = maxVal;
+        parents[p.second] = nullptr;
+        if(p.first != id)   order.push_back(p.first);
+    }
+
+    BellmanFordAlgorithm(distances, parents, order);
+    return std::make_pair(parents, distances);
+}
 
 #endif
